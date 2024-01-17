8ec54f9008e92ddc935f5cc9d867684c6e22e593
Kyrashup <104208981+Kyrashup@users.noreply.github.com>
2024-01-17 16:33:31 +1000

    Add files via upload

---
+++ file.ts
@@ -0,0 +1,23 @@
+const a: number = 1
+const b: string = "cfff"
+function  myFunction() {
+    const gfr = "znachenie peremennoi";
+    return "gfr"
+}
+const result:() =>string = myFunction;
+
+const massivStrok: string[] =["mama","papa","barat"];
+
+interface MyObject { 
+    name: string; 
+    age: number; 
+  } 
+   
+  const massiveObject: Array<MyObject> = [ 
+    { name: "mama", age: 20 }, 
+    { name: "papa", age: 30 }, 
+    { name: "barat", age: 40 }, 
+  ]; 
+   
+  console.log(massiveObject);
+
